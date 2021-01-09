e01eace22ff6538d7d43bb925ba87879a1f33129
Howard Yeend <howard.yeend@gmail.com>
2021-01-09 18:46:20 +0000

    editable islands!

--- src/App.js
+++ src/App.js
@@ -7,17 +7,12 @@ const Island = (props) => {
   const sumTiles = props.tiles.reduce((a, b) => a.concat(b)).reduce((p,c) => p + (c===1 ? 1 : 0));
   const code = props.code || IslandConverter.island2String(props.tiles);
   const href = `?i=${code}`;
-
-  const toggleTile = (x, y) => {
-    const newTiles = props.tiles;
-    newTiles[y][x] = newTiles[y][x] ? 0 : 1;
-    console.log(newTiles);
-  };
+  const islandClassName = props.editable ? 'layout-tiles editable': 'layout-tiles';
 
   return (<div className='layout' key={props.name}>
     <h3><a href={href}>{props.name}</a></h3>
     <div className='sum'>({sumTiles} tiles)</div>
-    <div className='layout-tiles'>
+    <div className={islandClassName}>
       {props.tiles.map((row, y) => <div className='tile-row' key={`tile-row-${y}`}>{row.map((cell,x) => {
         let className = 'tile';
         if (cell===1) {
@@ -26,7 +21,7 @@ const Island = (props) => {
           className += ' impassable';
         }
         const key = `tile-cell-${x},${y}`;
-        return <div onClick={() => toggleTile(x, y)} key={key} className={className} />
+        return <div onClick={() => props.editable ? props.toggleTile(x, y) : null} key={key} className={className} />
       })}</div>)}
     </div>
   </div>);
@@ -37,25 +32,46 @@ class App extends PureComponent {
   constructor(props) {
     super(props);
 
-    this.state = {
-      islands: Islands
+    const islands = {};
+    Islands.forEach(island => {
+      islands[island.name] = island;
+    });
+    const urlParams = new URLSearchParams(window.location.search);
+    const editIslandString = urlParams.get('i');
+    let editIsland = false;
+    if (editIslandString) {
+      const name = this.getIslandName(editIslandString);
+      editIsland = {
+        name,
+        code: editIslandString,
+        editable: true,
+        tiles: IslandConverter.string2island(editIslandString)
+      };
     }
+
+    this.state = { islands, editIsland };
   }
 
-  renderIslandEditor(islandString) {
-    const island = {
-      name: `Island ${islandString}`,
-      code: islandString,
-      editable: true,
-      tiles: IslandConverter.string2island(islandString)
-    };
+  getIslandName(code) {
+    return `Island ${code}`;
+  }
 
+  toggleTile = (x, y) => {
+    const editIsland = {...this.state.editIsland};
+    editIsland.tiles[y][x] = editIsland.tiles[y][x] ? 0 : 1;
+    editIsland.code = IslandConverter.island2String(editIsland.tiles);
+    editIsland.name = this.getIslandName(editIsland.code);
+    this.setState({ editIsland });
+  }
+
+  renderIslandEditor() {
     return (
       <React.Fragment>
         <h1>Forbidden Island Map Editor</h1>
         <div className='layouts'>
           <Island
-          {...island}
+          {...this.state.editIsland}
+          toggleTile={this.toggleTile}
         />
         </div>
       </React.Fragment>
@@ -63,16 +79,14 @@ class App extends PureComponent {
   }
 
   render() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const island = urlParams.get('i');
     return (
       <div className='islands'>
-        {island ? this.renderIslandEditor(island) : null}
+        {this.state.editIsland ? this.renderIslandEditor() : null}
         <h1>Sample Islands</h1>
         <div className='layouts'>
-          {this.state.islands.map(island => <Island
-            tiles={island.tiles}
-            name={`'${island.name}'`}
+          {Object.keys(this.state.islands).map(islandKey => <Island
+            tiles={this.state.islands[islandKey].tiles}
+            name={`'${this.state.islands[islandKey].name}'`}
             />)}
         </div>
       </div>
